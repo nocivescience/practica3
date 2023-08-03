9a117bf636ab805a9433b168f9142f8b93f245e7
ricardo <nocive.science@gmail.com>
2023-08-03 01:01:35 -0400

    listo1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/practica3/'
+  base: 'https://github.com/nocivescience/practica3'
 })
